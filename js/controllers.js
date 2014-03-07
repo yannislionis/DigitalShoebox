0590a36d1e39dc034a77d73046b3b45ccddfdbd4
Martin Schinz <m.schinz@gmail.com>
2014-03-07 09:24:40 +0000

    partial implementation of image preview

--- js/controllers.js
+++ js/controllers.js
@@ -14,4 +14,21 @@ shoeboxAppControllers.controller('TransactionDetailsController',
 
 			$scope.transaction =
 				MockData.get('', parseInt($routeParams.transactionId))
+
+			$scope.getFile = function () {
+				$scope.progress = 0;
+				fileReader.readAsDataUrl($scope.file, $scope)
+			.then(function(result) {
+				$scope.imageSource = result;
+			});
+			};
+
+			shoeboxApp.directive("ngFileSelect",function(){
+			return {
+				link: function($scope,el){
+						el.bind("change", function(e){
+								$scope.file = (e.srcElement || e.target).files[0];
+								$scope.getFile();
+							})
+					}}});
 }]);
